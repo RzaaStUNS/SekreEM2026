1654dd547003211fc8489e77cf8c458394d2dca6
RzaaStUNS <abimanyuriza008@student.uns.ac.id>
2026-02-07 14:00:54 +0700

    Update Sidebar.tsx

--- Frontend/src/components/layout/Sidebar.tsx
+++ Frontend/src/components/layout/Sidebar.tsx
@@ -58,7 +58,7 @@ const menuItems: MenuItem[] = [
   },
   {
     icon: <Calendar size={20} />,
-    label: "Kalender Proker",
+    label: "Kalender Proker Emailkomp",
     path: "/calendar",
   },
   {
